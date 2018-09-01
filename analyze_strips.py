bdb6d0dc7073c2aa1ad62639a4c343cddd0bf1bc
bjornburkle <bbbjornburkle@gmail.com>
2018-09-01 18:07:04 -0500

    classes working properly and can fully parse through labview input

--- analyze_strips.py
+++ analyze_strips.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python2.7
+
 import strip_analysis_classes as sistrip
 import sys
 import argparse
@@ -6,7 +8,12 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import numpy as np
 
-parser = argparse.ArgumentParser(description='Reads LabView strip measurement output file. Uses measurements to make plots and output formatted data to a text file. This file uses the custom classes written in "strip_analysis_classes.py"')
+if sys.version_info[0] == 3:
+    print '''This script runs using python 2.7 and you have a different version as the default.\n
+Run program using ./ rather than python to run using correct version of python'''
+    sys.exit()
+
+parser = argparse.ArgumentParser(description='''Reads LabView strip measurement output file. Uses measurements to make plots and output formatted data to a text file. This file uses the custom classes written in "strip_analysis_classes.py". I have tried to comment this piece of code a whole lot for anyone who may need to change it in the future :)''')
 parser.add_argument('file', help='Required input, tells script which text file to read')
 parser.add_argument('-r', '--root', help='''Create a ROOT file with the parsed measurements.
                     You must have the pyroot library installed to use this otion.
@@ -20,15 +27,29 @@ parser.add_argument('-k', '--keep_all', help='''If you have multiple measurement
 
 #if the LabView output file changes, this is the method which must be edited
 def readFile(filename, keep):
-    f = open(filename, "r").readlines()
+    file = open(filename, "r").read().splitlines()
+    #print file #this is nice for debugging stuff in the for in the while loop
+    f = iter(file)
     cc_offsent = 0.0
     ic_offset = 0.0
     sensor = sistrip.sensor()
     strip_meas = sistrip.strip()
     first_strip = True #used to determine if first strip or not
     iv = [] #used for calculating res of IV curves, bad notation because the form is [i,v]
-    for line in f:
+    #loop through the lines in the file and read the data
+    # rather than using a standard for loop, this script defines the list of lines "f"
+    # as a iterator and uses a while loop to manually loop through the lines
+    # the reason the code does it this way is because based on the content of a line of
+    # text I may need to access information on the next line(s). Because the number
+    # of measurements for a resistance measurements can be a variable number
+    # I did not want to use the zip command to hard code in the number of measurements
+    # I would take. This is just the fastest and easiest to understand way
+    # to do this that I could think of
+    line = f.next()
+    while True:
         iv = []
+        #print line
+        #print first_strip
         #look for open cap measurements
         if 'MHz' in line:
             if 'Not Measured' not in line:
@@ -43,39 +64,61 @@ def readFile(filename, keep):
                         ic_offset = float(line.split()[7])
                 else:
                     cc_offset = float(line.split()[5])
+            line = f.next()
+            continue
         #get strip number
         elif 'Strip' in line:
             #see if this is the first strip. If not, append the previous set of measurements to the "sensor" variable and clear the variable.
             if first_strip == False:
-                sensor.add_strip(strip, keep)
-                strip.clear()
-            else: first_strip = True
-            strip_meas.add_meas('strip', float(line.split()[1]))
+                sensor.add_strip(strip_meas, keep)
+                strip_meas.clear()
+            else: first_strip = False
+            strip_meas.add_meas('strip', int(line.split()[1]))
+            line = f.next()
+            continue
         #get chuck temperature
         elif 'ChuckT' in line:
             #the weird stuff in split is basically just telling it to split at the boundary of chars and numbers
             strip_meas.add_meas('chuckT', float(re.split('(\d+\.\d+)',line)[1]))
+            line = f.next()
+            continue
         #get air temperature
         elif 'airT' in line:
             strip_meas.add_meas('airT', float(re.split('(\d+\.\d+)',line)[1]))
+            line = f.next()
+            continue
         #get humidity
         elif 'Humidity' in line:
             strip_meas.add_meas('humid', float(re.split('(\d+\.\d+)',line)[1]))
+            line = f.next()
+            continue
         #get pinhole
         elif 'Pinhole' in line:
             strip_meas.add_meas('pinhole', float(line.split()[1]))
+            line = f.next()
+            continue
         #get coupling cap
         elif 'Coupling Cap' in line:
             strip_meas.add_meas('coupC', float(line.split()[2]) - cc_offset)
+            line = f.next()
+            continue
         #get interstrip capacitence
         elif 'Interstrip C' in line:
             strip_meas.add_meas('interC', float(line.split()[2]) - ic_offset)
+            line = f.next()
+            continue
         #get the global current
         elif 'Global IV' in line:
-            strip_meas.add_meas('bias', float(next(line)))
+            line = f.next()
+            strip_meas.add_meas('bias', float(line))
+            line = f.next()
+            continue
         #get the leakage current
         elif 'Ileak' in line:
-            strip_meas.add_meas('ileak', float(next(line).split()[1]))
+            line = f.next()
+            strip_meas.add_meas('ileak', float(line.split()[1]))
+            line = f.next()
+            continue
         #get poly resistance
         # for resistance measurements, you need to fit a straight line to an IV curve
         # the first data points used is the leakage current at V = 0
@@ -86,42 +129,47 @@ def readFile(filename, keep):
         elif 'RBias' in line:
             if strip_meas.get_meas('ileak') != 0:
                 iv.append([0.0, strip_meas.get_meas('ileak')])
-            i = 1
-            while True:
-                if line.next(i) != '\n':
-                    iv.append([float(line.next(i).split()[0]),float(line.next(i).split()[1])])
-                    i+=1
-                else: break
+            line = f.next()
+            while line != '':
+                iv.append([float(line.split('\t')[0]),float(line.split('\t')[1])])
+                line = f.next()
             strip_meas.add_meas('rbias', calc_res(iv))
+            continue
         #get leakage current from neighboring strip
         # note there is a typo from labview, if the typo is fixed
         # this elif statement needs to be fixed too
         elif 'DC Neihbor ILeak' in line:
-            strip_meas.add_meas('ileaknbr', float(line.next().split()[1]))
+            line = f.next()
+            strip_meas.add_meas('ileaknbr', float(line.split()[1]))
+            line = f.next()
+            continue
         elif 'RBNbr' in line:
             if strip_meas.get_meas('ileaknbr') != 0.0:
-                iv.append(0, strip_meas.get_meas('ileaknbr'))
-            i = 1
-            while True:
-                if line.next(i) != '\n':
-                    iv.append([float(line.next(i).split()[0]),float(line.next(i).split()[1])])
-                    i+=1
-                else: break
+                iv.append([0, strip_meas.get_meas('ileaknbr')])
+            line = f.next()
+            while line != '':
+                iv.append([float(line.split('\t')[0]),float(line.split('\t')[1])])
+                line = f.next()
             strip_meas.add_meas('rbiasnbr', calc_res(iv))
+            continue
         elif 'Interstrip R' in line:
-            i = 1
-            while True:
-                if line.next(i) != '\n':
-                    iv.append([float(line.next(i).split(0)),float(line.next(i).split()[1])])
-                else: break
+            line = f.next()
+            while line != '':
+                iv.append([float(line.split('\t')[0]),float(line.split('\t')[1])])
+                line = f.next()
             strip_meas.add_meas('interR', calc_res(iv))
+            continue
+        try:
+            line = f.next()
+        except StopIteration:
+            break
     return sensor
 
 #Makes an IV curve to calculate the resistance
 #For rbias, uses the leakage current as the point (ileak, 0V)
 def calc_res(iv):
-    volt = iv[0,:]
-    curr = iv[1,:]
+    volt = [elm[0] for elm in iv]
+    curr = [elm[1] for elm in iv]
     fit = np.polyfit(curr, volt, 1)
     return fit[0]
 
@@ -138,7 +186,8 @@ def makeRoot():
 def main():
     args=parser.parse_args()
     sensor = readFile(args.file, args.keep_all)
-    print sensor
+    for item in sensor.get_list():
+        print item
     if args.root:
         makeRoot()
 
--- strip_analysis_classes.py
+++ strip_analysis_classes.py
@@ -1,4 +1,4 @@
-import numpy as np
+#!/usr/bin/python2.7
 
 class strip:
 
@@ -18,29 +18,31 @@ class strip:
             'chuckT': 0.0
             }
 
-    def __init(self):
-        return self
-
-    #def __init__(self, num):
-    #    self.strips['strip'] = num
-
-
     def add_meas(self, meas, value):
         self.strips[meas] = value
 
-    def get(self, meas):
-        return self.strips['meas']
+    def get_meas(self, meas):
+        return self.strips[meas]
+
+    def get_all_meas(self):
+        return self.strips
     
     def clear(self):
-        for key in self:
+        for key in self.strips:
             if key == 'strip':
-                strips[key] = 0
+                self.strips[key] = 0
             else:
-                strips[key] = 0.0
+                self.strips[key] = 0.0
+
 
 class sensor:
+
+    #the fact that I sometimes use strip as a variable and 
+    #sometimes use it when refering to the previous class is
+    #pretty bad....sorry about that
     
     meas_taken = {
+            'strip': True,
             'bias': True,
             'ileak': True,
             'rbias': True,
@@ -55,44 +57,52 @@ class sensor:
             'chuckT': True
             }
 
-    def __init__(self):
-        self = []
+    strips = []
 
     def add_strip(self, strip_meas, keep = False):
-        if type(strip_meas) != strip:
+        if not isinstance(strip_meas, strip):
             print('Trying to append something other than a strip measurement')
             return
-        if keep == False and (strip for strip in self if strip['strip'] == strip_meas['strip']):
-            _replace_strip(self, strip_meas)
-        self.append(strip_meas)
+        #if keep == False and (strip_old for strip_old in self.strips if strip_old['strip'] == strip_meas['strip']):
+        #    _replace_strip(self, strip_meas.get_all_meas())
+        print strip_meas.get_meas('strip')
+        print strip_meas.get_all_meas()
+        self.strips.append(strip_meas.get_all_meas().copy())
+        print self.strips[0]['strip']
 
     def _replace_strip(self, strip_meas):
-        self[next((index for (index, strip) in enumerate(strip) if strip['strip'] == strip_meas['strip']), None)] = strip_meas
+        self.strips[next((index for (index, strip_old) in enumerate(self) if strip_old['strip'] == strip_meas['strip']), None)] = strip_meas.copy()
 
     def order(self):
-        temp = sorted(self, key=lambda strip: strip['self'])
-        self = temp
+        temp = sorted(self.strips, key=lambda strip_meas: strip_meas['self'])
+        self.strips = temp
 
     def reverse_strips(self):
-        total = len(self)
-        for strip in self:
-            strip['strip'] = total - strip['self']
+        total = len(self.strips)
+        for strip_meas in self.strips:
+            strip_meas['strip'] = total - strip_meas['strip']
 
     def get_meas_list(self, meas):
         temp_list = []
-        for strip in self:
-            temp_list.append(strip['strip'], strip[meas])
+        if self.meas_taken[meas] == False:
+            print meas, 'measurement not taken'
+            return
+        for strip_meas in self.strips:
+            temp_list.append([strip_meas['strip'], strip_meas[meas]])
         temp_list.sort(key=lambda x: x[0])
-        self = temp_list
+        return temp_list
 
     def check_measurements(self):
-        if key = []:
+        if key == []:
             print 'No strip data was parsed'
             return
-        for key in self[0]:
-            temp = self.get_meas_list(key)
+        for key in self.strips[0]:
+            temp = strips.get_meas_list(key)
             if sum(temp[1,:]) == 0:
-                meas_taken[key] = False
+                self.meas_taken[key] = False
+
+    def get_list(self):
+        return self.strips
 
 
 def main():
